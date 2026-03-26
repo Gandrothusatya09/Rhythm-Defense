6c2d2a0097e9128cf28e1773121cb9d4d6c81c3e
Yamini <burugupalliyaminidevi@gmail.com>
2026-03-26 11:36:03 +0530

    added bomb ,garbage Collector ,Condition Bar, Player Movement scripts

---
+++ Assets/NewScripts/GarbageCollector.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class GarbageCollector : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Garbage"))
+        {
+            Debug.Log("Garbage Collected");
+
+            FindObjectOfType<ConditionBar>().AddCondition();
+
+            Destroy(other.gameObject); 
+        }
+    }
+}
